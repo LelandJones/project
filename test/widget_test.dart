880514af16ca74affe7fd36bc12f341bfbde5d5e
LelandJones <lelandj7@outlook.com>
2025-04-29 21:23:22 -0400

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cs386final/bowlingmain.dart';
+import 'package:cs386final/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
